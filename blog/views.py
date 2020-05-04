b0dbd5af850a5c2ab4c51e87a733551adbd5d0ea
Max <victor-kovalov@yandex.ru>
2020-05-04 12:59:14 +0300

    Get post's by similarity

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, View, FormView
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import EmailMessage
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -11,39 +12,44 @@ from blog.forms import EmailPostForm, CommentForm
 
 
 class PostDetail(FormView):
-    def get(self, request, *args, **kwargs):
-        post = get_object_or_404(Post,
+    @staticmethod
+    def get_post(**kwargs):
+        return get_object_or_404(Post,
                                  slug=kwargs['post'],
                                  status='published',
                                  publish__year=kwargs['year'],
                                  publish__month=kwargs['month'],
                                  publish__day=kwargs['day'])
 
-        comments = post.comments.filter(active=True)
+    @staticmethod
+    def get_similar_post(post):
+        post_tags_ids = post.tags.values_list('id', flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+        return similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
 
+    def get(self, request, *args, **kwargs):
+        post = self.get_post(**kwargs)
+        comments = post.comments.filter(active=True)
         comment_form = CommentForm()
+        similar_posts = self.get_similar_post(post)
         return render(request, 'blog/post/detail.html', {'post': post,
                                                          'comments': comments,
-                                                         'comment_form': comment_form})
+                                                         'comment_form': comment_form,
+                                                         'similar_posts': similar_posts})
 
     def post(self, request, *args, **kwargs):
-        post = get_object_or_404(Post,
-                                 slug=kwargs['post'],
-                                 status='published',
-                                 publish__year=kwargs['year'],
-                                 publish__month=kwargs['month'],
-                                 publish__day=kwargs['day'])
-
+        post = self.get_post(**kwargs)
         comments = post.comments.filter(active=True)
-
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
             new_comment.save()
+        similar_posts = self.get_similar_post(post)
         return render(request, 'blog/post/detail.html', {'post': post,
                                                          'comments': comments,
-                                                         'comment_form': comment_form})
+                                                         'comment_form': comment_form,
+                                                         'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
@@ -54,15 +60,18 @@ class PostListView(ListView):
 
 
 class PostShareView(FormView):
+    @staticmethod
+    def get_post(**kwargs):
+        return get_object_or_404(Post, id=kwargs['post_id'], status='published')
+
     def get(self, request, *args, **kwargs):
-        post = get_object_or_404(Post, id=kwargs['post_id'], status='published')
+        post = self.get_post(**kwargs)
         form = EmailPostForm()
         sent = False
         return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
     def post(self, request, *args, **kwargs):
-        post = get_object_or_404(Post, id=kwargs['post_id'], status='published')
-
+        post = self.get_post(**kwargs)
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
@@ -71,7 +80,6 @@ class PostShareView(FormView):
             message = f"Read {post.title} at {post_url}\n\n{cd['name']}\'s comments: {cd['comments']}"
             msg = EmailMessage(subject, message, to=['000.maximtretyakov.000@gmail.com'])
             msg.send(fail_silently=False)
-
         sent = True
         return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
@@ -79,13 +87,11 @@ class PostShareView(FormView):
 class PostListPagination(View):
     def get(self, request, *args, **kwargs):
         object_list = Post.published.all()
-
         tag = None
         if kwargs['tag_slug']:
             tag = get_object_or_404(Tag, slug=kwargs['tag_slug'])
             object_list = object_list.filter(tags__in=[tag])
-
-        paginator = Paginator(object_list, 3)  # 3 posts in each page
+        paginator = Paginator(object_list, 3)
         page = request.GET.get('page')
         try:
             posts = paginator.page(page)
